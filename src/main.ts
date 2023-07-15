62a0457efb3a178022148e08f68c1c7b5233d91d
darker <darkerros@yandex.ru>
2023-07-16 01:56:11 +0300

    feat: add global validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
+import * as process from 'process';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import * as process from "process";
+
+import { ValidationPipe } from '@nestjs/common';
+import { validationExceptionFactory } from './factory/validation-exception-factory';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(
+    new ValidationPipe({ exceptionFactory: validationExceptionFactory }),
+  );
+
   const APP_PORT = process.env.PORT;
   await app.listen(APP_PORT, () => {
-    console.log(`Start on: ${APP_PORT}`)
+    console.log(`Start on: ${APP_PORT}`);
   });
 }
 
